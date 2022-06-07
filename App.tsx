ac50b64652e0773ca16b967e1ee89d6219bb1a27
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-07 19:09:01 -0300

    feat: adjusting the status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Home } from "./src/screens/home";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 
 export default function App() {
@@ -9,6 +10,9 @@ export default function App() {
   if (!fontLoaded) { return <AppLoading /> }
 
   return (
-    <Home />
+    <>
+      <StatusBar style="light"/>
+      <Home />
+    </>
   );
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,7 +1,9 @@
 import { StyleSheet } from "react-native";
+import { color } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
+    backgroundColor: color.blackSecondary,
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
