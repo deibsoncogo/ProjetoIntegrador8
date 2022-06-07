9cf329b2828957b4492cfb797b2ff807191e718f
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-06 22:08:01 -0300

    feat: implementation fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { Home } from "./src/screens/home";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
+
 
 export default function App() {
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontLoaded) { return <AppLoading /> }
+
   return (
     <Home />
   );
