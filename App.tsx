220377e9dd578e0c3e5da3f1c3f896a083103049
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-12 14:36:03 -0300

    dependency: remove expo-app-loading and expo-google-fonts/roboto

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import { Home } from "./src/screens/home";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
 
 
 export default function App() {
-  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
-  if (!fontLoaded) { return <AppLoading /> }
-
   return (
     <>
       <StatusBar style="light"/>
