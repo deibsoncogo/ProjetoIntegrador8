f23b4d883a1f546d540a23ca74087fea04e62d57
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-22 08:34:54 -0300

    database backend: implementation table patients

---
+++ backend/prisma/migrations/20220622113002_patients/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Patients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birth_date" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "rg" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patients_cpf_key" ON "Patients"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patients_rg_key" ON "Patients"("rg");
